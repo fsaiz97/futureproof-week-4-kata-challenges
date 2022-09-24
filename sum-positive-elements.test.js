e2f58c43f831f308dc5d977624ffbc940622c71d
fsaiz97 <felixsaiz97@gmail.com>
2022-09-24 16:38:37 +0100

    Add positive tests

--- sum-positive-elements.test.js
+++ sum-positive-elements.test.js
@@ -1,5 +1,14 @@
 const { sumPositiveElements } = require("./sum-positive-elements");
+const each = require("jest-each").default;
 
 describe("sumPositiveElements tests", () => {
-    
+    each([
+        [10, [1, 2, 3, 4]],
+        [104, [1, -4, 3, -10, 100]],
+        [0, [0, 0, 0]],
+        [0, [-1, -2, -3, -4]],
+        [0, []]
+    ]).test("Returns %s when passed %s", (expected, arr) => {
+        expect(sumPositiveElements(arr)).toBe(expected);
+    })
 })
