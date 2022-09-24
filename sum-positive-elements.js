ca5bfdc9db4adea34e7edc2aaf87da6621746524
fsaiz97 <felixsaiz97@gmail.com>
2022-09-24 17:40:53 +0100

    Add negative error throwing tests Make function pass tests

--- sum-positive-elements.js
+++ sum-positive-elements.js
@@ -1,5 +1,19 @@
 function sumPositiveElements(arr) {
-    
+    let expectedNumOfParams = 1;
+    if (arguments.length !== expectedNumOfParams) {
+        throw new Error(`Function takes 1 argument. ${arguments.length} recieved.`);
+    } else if (!Array.isArray(arr)) {
+        throw new Error("Function expects an array argument.");
+    } else if (!arr.every(element => typeof element === "number")) {
+        throw new Error("Function expects an array of numbers.");
+    }
+
+    let sum = 0;
+    sum += arr.filter(element => element > 0).reduce((prev, curr) => prev + curr, 0);
+
+    return sum
 }
+
+sumPositiveElements([1,2,3])
 
 module.exports = { sumPositiveElements }
--- sum-positive-elements.test.js
+++ sum-positive-elements.test.js
@@ -2,6 +2,48 @@ const { sumPositiveElements } = require("./sum-positive-elements");
 const each = require("jest-each").default;
 
 describe("sumPositiveElements tests", () => {
+    it("Throws error when called with no arguments", () => {
+        expect(() => {
+            sumPositiveElements()
+        }).toThrow(new Error("Function takes 1 argument. 0 recieved."))
+    })
+
+    it("Throws error when called with too many arguments", () => {
+        expect(() => {
+            sumPositiveElements([1, 2], [3, 4])
+        }).toThrow(new Error("Function takes 1 argument. 2 recieved."))
+    })
+
+    it("Throws an error when the argument is not an array", () => {
+        expect(() => {
+            sumPositiveElements("array")
+        }).toThrow(new Error("Function expects an array argument."))
+        expect(() => {
+            sumPositiveElements(10)
+        }).toThrow(new Error("Function expects an array argument."))
+        expect(() => {
+            sumPositiveElements({"array": 10, "false": -10})
+        }).toThrow(new Error("Function expects an array argument."))
+    })
+
+    it("Throws an error if any of the array's elements are not a number", () => {
+        expect(() => {
+            sumPositiveElements(['1', 2, 4])
+        }).toThrow(new Error("Function expects an array of numbers."))
+        expect(() => {
+            sumPositiveElements([1, [1, 2], 4])
+        }).toThrow(new Error("Function expects an array of numbers."))
+        expect(() => {
+            sumPositiveElements([1, 2, "abc", 4])
+        }).toThrow(new Error("Function expects an array of numbers."))
+        expect(() => {
+            sumPositiveElements([true, 2, 4])
+        }).toThrow(new Error("Function expects an array of numbers."))
+        expect(() => {
+            sumPositiveElements(['1', 2, {"name": 10}])
+        }).toThrow(new Error("Function expects an array of numbers."))
+    })
+
     each([
         [10, [1, 2, 3, 4]],
         [104, [1, -4, 3, -10, 100]],
