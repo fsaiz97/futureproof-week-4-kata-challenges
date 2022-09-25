031a37374eadd506b2b8ae6dd83984615c5250fa
fsaiz97 <felixsaiz97@gmail.com>
2022-09-25 11:30:52 +0100

    Write function Function passes all tests

--- reverse-string.js
+++ reverse-string.js
@@ -1,5 +1,16 @@
-function reverseString() {
+function reverseString(str) {
+    if (arguments.length !== 1) {
+        throw new Error(`This function expects 1 argument, ${arguments.length} recieved.`)
+    } else if (typeof str !== "string") {
+        throw new Error("This function expects a string argument.");
+    }
 
+    let result = "";
+    for (let i = (str.length - 1); i >= 0; i--) {
+        result += str[i];
+    }
+
+    return result
 }
 
 module.exports = { reverseString };
